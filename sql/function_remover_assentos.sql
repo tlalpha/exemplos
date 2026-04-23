f6bbf7c57fd3b6faf242cb2d6240557a13779948
tlalpha <83185074+tlalpha@users.noreply.github.com>
2026-04-23 17:38:02 -0400

    Resolvido bug. Versão funcional

--- sql/function_remover_assentos.sql
+++ sql/function_remover_assentos.sql
@@ -5,9 +5,8 @@ BEGIN
     -- Se o texto de entrada for NULO, retorna NULO.
     IF @Texto IS NULL
         RETURN NULL;
-
     -- A conversão para um tipo de dados não-Unicode (VARCHAR) com uma collation
     -- Accent-Insensitive (AI) força a remoção dos acentos.
-    RETURN CONVERT(VARCHAR(MAX), @Texto COLLATE SQL_Latin1_General_CP1_CI_AI);
+    RETURN CONVERT(NVARCHAR(MAX), CONVERT(VARCHAR(MAX), @Texto COLLATE SQL_Latin1_General_CP1251_CI_AS));
 END;
 GO
